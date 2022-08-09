f7f6afa6b67a63143b5c8e2a6727c1b98547c0df
zollbam <whrjsdud93@naver.com>
2022-08-10 08:42:13 +0900

    t

--- test.R
+++ test.R
@@ -57,7 +57,7 @@ h1_df_ho_to<-df_ho%>%
     summarise(total=sum(count)/2)
 result<-cbind(h0_df_ho_to,h1_df_ho_to)
 names(result)<-c('ho','h1')
-
+result
 par(mfrow=c(1,2))
 barplot(result$ho,xlab='ho')
 barplot(result$h1,xlab='h1')
